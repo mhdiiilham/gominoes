acf343bb4a202a2a95923ea2264d2d0473a4830a
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-12-27 15:14:40 +0700

    feat(main.go): setup custom validator

--- go.mod
+++ go.mod
@@ -4,6 +4,10 @@ go 1.15
 
 require (
 	github.com/go-playground/assert v1.2.1
+	github.com/go-playground/locales v0.13.0
+	github.com/go-playground/universal-translator v0.17.0
 	github.com/gofiber/fiber/v2 v2.3.0
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/stretchr/testify v1.6.1
+	gopkg.in/go-playground/validator.v9 v9.31.0
 )
--- main.go
+++ main.go
@@ -3,10 +3,38 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
 	"github.com/gofiber/fiber/v2"
+	"gopkg.in/go-playground/validator.v9"
+	en_translations "gopkg.in/go-playground/validator.v9/translations/en"
 )
 
 func main() {
+	translator := en.New()
+	uni := ut.New(translator, translator)
+
+	trans, found := uni.GetTranslator("en")
+	if !found {
+		panic("Translator not found!")
+	}
+	v := validator.New()
+	if err := en_translations.RegisterDefaultTranslations(v, trans); err != nil {
+		panic(err)
+	}
+	_ = v.RegisterTranslation("required", trans, func(ut ut.Translator) error {
+		return ut.Add("required", "{0} is required field", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("required", fe.Field())
+		return t
+	})
+	_ = v.RegisterTranslation("email", trans, func(ut ut.Translator) error {
+		return ut.Add("email", "{0} must be a valid email", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("email", fe.Field())
+		return t
+	})
+
 	app := setupApp()
 	app.Listen(":3000")
 }
