7c46d41f10d4b81d3ee6dac39f5be7f699950154
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-12-30 21:55:56 +0700

    feat(api/app/app.go): create fiber custom error handler

--- api/app/app.go
+++ api/app/app.go
@@ -8,7 +8,9 @@ import (
 
 // SetupApp fiber route
 func SetupApp() *fiber.App {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		ErrorHandler: customErrHandler,
+	})
 	app.Get("/", func(c *fiber.Ctx) error {
 		msg := map[string]string{
 			"message": "GO-MINOES",
@@ -16,4 +18,34 @@ func SetupApp() *fiber.App {
 		return c.Status(http.StatusOK).JSON(msg)
 	})
 	return app
+}
+
+func customErrHandler(ctx *fiber.Ctx, err error) error {
+	code := http.StatusInternalServerError
+	msg := "Internal Server Error"
+
+	if e, ok := err.(*fiber.Error); ok {
+		code = e.Code
+		msg = e.Message
+	}
+
+	err = ctx.Status(code).JSON(struct {
+		Code    int
+		Message string
+	}{
+		Code:    code,
+		Message: msg,
+	})
+
+	if err != nil {
+		return ctx.Status(500).JSON(struct {
+			Code int
+			Msg  string
+		}{
+			Code: 500,
+			Msg:  "Fuck",
+		})
+	}
+
+	return nil
 }
