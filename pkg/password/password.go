7e49d5b37d51552e3ea6a41bdae87272e699c704
Muhammad Ilham <muhd.iiilham@gmail.com>
2021-01-07 01:00:19 +0700

    refactor(pkg/password/password.go): To A Testable Code
    refactor password package to a testable code.
    Create new struct called `BcryptHash`, this struct
    will be create through function `NewBCryptHash` and then
    pass to user manager. Interface `Hasher` created to make
    test is easier by mocking the interface later on

--- pkg/password/password.go
+++ pkg/password/password.go
@@ -2,14 +2,28 @@ package password
 
 import "golang.org/x/crypto/bcrypt"
 
+// Hasher interface
+type Hasher interface {
+	Hash(password string) string
+	Compare(plainPassword, hashPassword string) bool
+}
+
+// BcryptHash struct
+type BcryptHash struct{}
+
+// NewBCryptHash return Hasher interface
+func NewBCryptHash() Hasher {
+	return &BcryptHash{}
+}
+
 // Hash Password function
-func Hash(password string) string {
+func (b *BcryptHash) Hash(password string) string {
 	hb, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
 	return string(hb)
 }
 
 // Compare Password function
-func Compare(plainPassword, hashPassword string) bool {
+func (b *BcryptHash) Compare(plainPassword, hashPassword string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte(plainPassword))
 	return err == nil
 }
